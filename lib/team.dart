ab8ac57cf722f2693b6eff52d7e1af7d712c1632
Steve Mactaggart <steve@whitesquaresoft.com>
2019-01-17 22:09:06 +1100

    Updated the team list with actual team members.

--- lib/team.dart
+++ lib/team.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:meta/meta.dart';
+import 'package:yaml/yaml.dart';
+
 // @required is defined in the meta.dart package
 
 // We use an underscore to indicate that these variables are private.
 // See https://www.dartlang.org/guides/language/effective-dart/design#libraries
 final _rowHeight = 100.0;
 
-class Team extends StatelessWidget {
+class TeamMember {
+  final String name;
+  final String image;
+  final DateTime joined;
+
+  TeamMember({this.name, this.image, this.joined});
+
+  factory TeamMember.fromJson(YamlMap json) {
+    var image;
+    if (json["avatar"] != null) {
+      image = "assets/" + json['avatar'];
+    }
+
+    var joined;
+    if (json["joined"] != null) {
+      joined = DateTime.parse(json['joined']);
+    }
+
+    return TeamMember(name: json['name'], image: image, joined: joined);
+  }
+}
+
+class TeamMemberPanel extends StatelessWidget {
   final Image image;
   final String name;
   final DateTime joined;
 
-  const Team({
+  const TeamMemberPanel({
     Key key,
     @required this.image,
     @required this.name,
@@ -80,54 +104,82 @@ class Team extends StatelessWidget {
 class TeamRoute extends StatelessWidget {
   const TeamRoute();
 
-  @override
-  Widget build(BuildContext context) {
-    final categories = [];
+  _loadTeam(BuildContext context) async {
+    final data =
+        await DefaultAssetBundle.of(context).loadString("assets/team.yaml");
 
-    final images = [
-      "assets/owen_chung.jpg",
-      "assets/paul_borg.jpg",
-      "assets/steve_mactaggart.jpg",
-      "assets/trent_hornibrook.jpg"
-    ];
+    var teamYaml = loadYaml(data);
 
-    final names = [
-      "Owen Chung",
-      "Paul Borg",
-      "Steve Mactaggart",
-      "Trent Hornibrook"
-    ];
+    return teamYaml["people"].map((yaml) {
+      return TeamMember.fromJson(yaml);
+    }).toList();
+  }
 
-    for (var i = 0; i < 35; i++) {
-      categories.add(
-        Center(
-          child: InkWell(
-            onTap: () {
-              print("clicked");
-            },
-            child: Card(
-              color: Colors.white,
-              child: Team(
-                name: names[i % 4],
-                joined: DateTime.now(),
-                image: new Image(
-                    image: new AssetImage(images[i % 4]),
-                    fit: BoxFit.contain,
-                    width: 50.0),
+  Widget _buildPage(List teamMembers) {
+    return ListView(
+        shrinkWrap: true,
+        primary: false,
+        children: teamMembers.map((teamMember) {
+          var image;
+          if (teamMember.image == null) {
+            image = new AssetImage("assets/DEFAULT_PERSON.jpg");
+          } else {
+            try {
+              image = new AssetImage(teamMember.image);
+            } catch (e) {
+              print(e);
+              image = new AssetImage("assets/DEFAULT_PERSON.jpg");
+            }
+          }
+
+          return Center(
+            child: InkWell(
+              onTap: () {
+                print("clicked");
+              },
+              child: Card(
+                color: Colors.white,
+                child: TeamMemberPanel(
+                  name: teamMember.name,
+                  joined: teamMember.joined,
+                  image:
+                      new Image(image: image, fit: BoxFit.contain, width: 50.0),
+                ),
               ),
             ),
-          ),
-        ),
-      );
-    }
+          );
+        }).toList());
+  }
 
+  @override
+  Widget build(BuildContext context) {
     final listView = Container(
-      color: Colors.orangeAccent,
-      child: ListView.builder(
-        itemBuilder: (BuildContext context, int index) => categories[index],
-        itemCount: categories.length,
-      ),
-    );
+        color: Colors.orangeAccent,
+        child: ListView.builder(itemBuilder: (context, pageNumber) {
+          return FutureBuilder(
+            future: this._loadTeam(context),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return SizedBox(
+                    height: MediaQuery.of(context).size.height * 2,
+                    child: Align(
+                        alignment: Alignment.topCenter,
+                        child: CircularProgressIndicator()),
+                  );
+                case ConnectionState.done:
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    var pageData = snapshot.data;
+
+                    return this._buildPage(pageData);
+                  }
+              }
+            },
+          );
+        }));
 
     final appBar = AppBar(
       elevation: 0.0,
